384d892cb67c6ef97b58b42732db00d4208bd564
Alan Paulo dos santos <onloadtecnologia@gmail.com>
2022-05-18 20:13:16 -0300

    second commit

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import logo from '../../logo.svg'
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className="row">
-            <div className="col-12">
-                <p className="text-center">Home</p>
-            </div>
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <img src={logo} alt="" srcset="" />
+            <p className="text-center">Home</p>
         </div>
     )
 }
